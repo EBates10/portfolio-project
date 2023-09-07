58175413950d23c25ba9bce30fe83d1e26799c20
Ebates10 <emilyspbates@gmail.com>
2023-09-06 20:16:31 -0400

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>HomePage.vue</h1>
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
